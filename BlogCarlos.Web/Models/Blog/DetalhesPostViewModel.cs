933bb1f3b9e2a30cfd4bde24c122a9752df9bb25
Carlos Cardoso Furtado Junior <carlos.furtado@zenthi.com.br>
2015-12-16 19:27:47 -0200

    Terceira Checked

--- BlogCarlos.Web/Models/Blog/DetalhesPostViewModel.cs
+++ BlogCarlos.Web/Models/Blog/DetalhesPostViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -16,5 +18,30 @@ namespace BlogCarlos.Web.Models.Blog
         public string Resumo { get; set; }
         public bool Visivel { get; set; }
         public List<string> Tags { get; set; }
+
+        /*CADASTRAR COMENTARIO*/
+        [DisplayName("Nome")]
+        [StringLength(100, ErrorMessage = " O campo Nome deve possuir no máximo {1} carateres")]
+        [Required(ErrorMessage = "* O campo Nome é obrigatório.")]
+        public string ComentarioNome { get; set; }
+
+        [DisplayName("E-mail")]
+        [StringLength(100, ErrorMessage = " O campo E-mail deve possuir no máximo {1} carateres")]
+        [EmailAddress(ErrorMessage ="E-mail inválido")]
+        public string ComentarioEmail { get; set; }
+
+        [DisplayName("Descrição")]
+        [Required(ErrorMessage = "O campo Descrição é obrigatório.")]
+        public string ComentarioDescricao { get; set; }
+
+        [DisplayName("Página Web")]
+        [Required(ErrorMessage = "O campo Página Web é obrigatório.")]
+        public string ComentarioPagina { get; set; }
+
+
+
     }
+
+}
+
 }
